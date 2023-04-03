2a23dab5a2d6f26159aa40abf835f5d211e21691
Ingo Meyer <IJ_M@gmx.de>
2023-04-03 12:43:50 +0200

    Do not abort when `tput` is not available

--- src/terminal.sh
+++ src/terminal.sh
@@ -11,21 +11,34 @@ check_supported_colors () {
 }
 
 init_terminal_formatting () {
-    command_available tput || return
+    local escape
 
-    # shellcheck disable=SC2034
-    TERM_RESET="$(tput sgr0)"
-    # shellcheck disable=SC2034
-    TERM_BOLD="$(tput bold)"
-    # TERM_FG_BLACK="$(tput setaf 0)"
-    # TERM_FG_RED="$(tput setaf 1)"
-    # TERM_FG_GREEN="$(tput setaf 2)"
-    # TERM_FG_YELLOW="$(tput setaf 3)"
-    # TERM_FG_BLUE="$(tput setaf 4)"
-    # TERM_FG_MAGENTA="$(tput setaf 5)"
-    # shellcheck disable=SC2034
-    TERM_FG_CYAN="$(tput setaf 6)"
-    # TERM_FG_WHITE="$(tput setaf 7)"
+    if command_available tput; then
+        TERM_RESET="$(tput sgr0)"
+        # TERM_BOLD="$(tput bold)"
+        # TERM_FG_BLACK="$(tput setaf 0)"
+        # TERM_FG_RED="$(tput setaf 1)"
+        # TERM_FG_GREEN="$(tput setaf 2)"
+        # TERM_FG_YELLOW="$(tput setaf 3)"
+        # TERM_FG_BLUE="$(tput setaf 4)"
+        # TERM_FG_MAGENTA="$(tput setaf 5)"
+        # TERM_FG_CYAN="$(tput setaf 6)"
+        # TERM_FG_WHITE="$(tput setaf 7)"
+    else
+        >&2 echo "Could not query the terminal database, assume an xterm compatible terminal."
+        escape="$(printf "\033")"
+        # shellcheck disable=SC2034
+        TERM_RESET="${escape}[0m"
+        # TERM_BOLD="${escape}[1m"
+        # TERM_FG_BLACK="${escape}[30m"
+        # TERM_FG_RED="${escape}[31m"
+        # TERM_FG_GREEN="${escape}[32m"
+        # TERM_FG_YELLOW="${escape}[33m"
+        # TERM_FG_BLUE="${escape}[34m"
+        # TERM_FG_MAGENTA="${escape}[35m"
+        # TERM_FG_CYAN="${escape}[36m"
+        # TERM_FG_WHITE="${escape}[37m"
+    fi
 }
 
 get_cursor_column () {
@@ -40,6 +53,18 @@ get_cursor_column () {
 }
 
 get_terminal_width () {
-    command_available tput || return
-    tput cols
+    if command_available tput; then
+        tput cols
+        return
+    fi
+    if [[ -n "${COLUMNS}" ]]; then
+        echo "${COLUMNS}"
+        return
+    fi
+    init_script_tmp_dir || return
+    if [[ ! -f "${SCRIPT_TMP_DIR}/showed_term_width_warning" ]]; then
+        >&2 echo "Could not query terminal width, assume 80 columns."
+        touch "${SCRIPT_TMP_DIR}/showed_term_width_warning"
+    fi
+    echo "80"
 }
